d378abcec54fd2b35500721b4d2ee13b9ba30ae8
jaisun <jaisunr@icloud.com>
2023-05-11 14:06:23 -0700

    Commit from Macos

---
+++ Home.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+
+col1 , empty_col, col2 = st.columns([1.5, 0.5, 1.5])
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Jaisun Rattu")
+    content = """Welcome to my portfolio! My name is Jaisun Rattu and I'm a computer science student. I have always 
+    been passionate about technology and its ability to transform the world we live in. Through my coursework and 
+    personal projects, I've developed skills in software development, including experience with programming languages 
+    such as Python and Java. This portfolio showcases some of my best work and provides an overview of my skills and 
+    experience. I'm excited to continue learning and growing in this field, and I look forward to 
+    the opportunities that lie ahead. Thank you for visiting my portfolio!"
+    """
+    st.info(content)
+
+content2 = """
+Below you can find some apps I have built in Python. Feel free to contact me!
+"""
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
---
+++ README.md
@@ -0,0 +1,2 @@
+# What is this project?
+A website built entirely in Python to showcase coding projects and apps.
